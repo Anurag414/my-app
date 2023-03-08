1a56891c581fd4c358e82efa43bf9a756052b4fe
Anurag Pradhan <anurag9621p@gmail.com>
2023-03-08 11:00:02 +0530

    added: 2 new dropdown

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
 
-function App() {
+ export default function CarSelector() {
+  const [carTypes, setCarTypes] = useState([]);
+  const [carModels, setCarModels] = useState([]);
+  const [selectedCarType, setSelectedCarType] = useState(null);
+  const [selectedCarModel, setSelectedCarModel] = useState(null);
+
+  useEffect(() => {
+    // Fetch car types from API
+    fetch("https://server.sakshambhatt1.repl.co/car-type-options")
+      .then(response => response.json())
+      .then(data => {
+        setCarTypes(data);
+        setSelectedCarType(data.find(carType => carType.isDefault));
+      })
+      .catch(error => console.error(error));
+  }, []);
+
+  useEffect(() => {
+    if (selectedCarType) {
+      // Fetch car models for the selected car type from API
+      fetch(`https://server--sakshambhatt1.repl.co//car-options/suv${selectedCarType.id}`)
+        .then(response => response.json())
+        .then(data => {
+          setCarModels(data);
+          setSelectedCarModel(data.find(carModel => carModel.isDefault));
+        })
+        .catch(error => console.error(error));
+    }
+  }, [selectedCarType]);
+
+  function handleCarTypeChange(event) {
+    const carType = carTypes.find(carType => carType.id === event.target.value);
+    setSelectedCarType(carType);
+  }
+
+  function handleCarModelChange(event) {
+    const carModel = carModels.find(carModel => carModel.id === event.target.value);
+    setSelectedCarModel(carModel);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <label htmlFor="car-type"> Select Car Type:</label>
+      <select id="car-type" value={selectedCarType?.id} onChange={handleCarTypeChange}>
+        {carTypes.map(carType => (
+          <option key={carType.id} value={carType.id}>
+            {carType.name}
+          </option>
+        ))}
+      </select>
+      <br />
+      <label htmlFor="car-model">Select Car Model:</label>
+      <select id="car-model" value={selectedCarModel?.id} onChange={handleCarModelChange}>
+        {carModels.map(carModel => (
+          <option key={carModel.id} value={carModel.id}>
+            {carModel.name}
+          </option>
+        ))}
+      </select>
     </div>
   );
 }
-
-export default App;
